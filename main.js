249d38b58145c25a07e7d5703b252747d0f4042f
PRATIKLAHANE-77 <pratik.lahane1999@gmail.com>
2023-09-01 14:03:25 +0530

    learned body parser

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-const http = require('http');
-
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware');
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.urlencoded({extended:false}));
+
+
+app.use('/',(req, res, next) => {
+    console.log("this always runs");
     next();
+  });
 
-})
+app.use('/app-product',(req, res, next) => {
+    console.log("in another of anoter midleware");
+    res.send('<form action = "/product" method = "post"><input type = "text" name = "product name"><input type = "number" name = "size" <button type = "submit"><button>Add Product</button> </form>');
+  });
 
-app.use((req,res,next)=>{
-    console.log('in another midleware');
-})
+  app.use('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+  });
+
+  app.use('/',(req, res, next) => {
+    console.log("check");
+    res.send('<h1>Hello, welcome to express js</h1>')
+  });
 
 app.listen(4500);
